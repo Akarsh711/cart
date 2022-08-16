c41aa93496a028883b4f0e682deacb388cc374c5
Deepak-nishad <deepakkumar62559@gmail.com>
2022-08-16 10:45:32 +0530

    set state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       price: 999,
@@ -13,8 +13,34 @@ class CartItem extends React.Component {
   }
   increaseQuantity = () => {
     console.log('this', this.state);
+
+    // this.state.qty += 1;
+    // console.log('this', this.state);
+    // setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // });
+
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
+    
   }
-  render () {
+
+  decreaseQuantity = () => {
+    console.log('this', this.state);
+
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
+    });
+  }
+
+  render() {
     const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
@@ -22,9 +48,9 @@ class CartItem extends React.Component {
           <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>Rs {price} </div>
-          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: '#777' }}>Rs {price} </div>
+          <div style={{ color: '#777' }}>Qty: {qty} </div>
           <div className="cart-item-actions">
             {/* Buttons */}
             <img
@@ -37,6 +63,7 @@ class CartItem extends React.Component {
               alt="decrease"
               className="action-icons"
               src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+              onClick={this.decreaseQuantity}
             />
             <img
               alt="delete"
