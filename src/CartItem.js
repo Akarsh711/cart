7b8cd5b9c6f592be2dc417ea172914f6cf5f9f7e
Deepak-nishad <deepakkumar62559@gmail.com>
2022-08-21 09:56:31 +0530

    product render with map and provide unique id

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,25 +1,31 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: ''
-    }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-  }
-  increaseQuantity = () => {
-    console.log('this', this.state);
+  // testing () {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve('done');
+  //     }, 5000);
+  //   })
 
+  //   promise.then(() => {
+  //     // setState acts like a synchronus call
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     console.log('state', this.state);
+  //   });
+  // }
+  increaseQuantity = () => {
     // this.state.qty += 1;
     // console.log('this', this.state);
     // setState form 1
     // this.setState({
     //   qty: this.state.qty + 1
-    // });
+    // }, () => {});
 
     // setState form 2 - if prevState required use this
     this.setState((prevState) => {
@@ -27,35 +33,34 @@ class CartItem extends React.Component {
         qty: prevState.qty + 1
       }
     });
-
-   
-    
   }
 
   decreaseQuantity = () => {
-    // console.log('this', this.state); 
-    const{qty}=this.state;
-     if(qty===0){
+    const { qty } = this.state;
+
+    if (qty === 0) {
       return;
-     }
+    }
+    // setState form 2 - if prevState required use this
     this.setState((prevState) => {
       return {
         qty: prevState.qty - 1
       }
     });
   }
-
-  render() {
-    const { price, title, qty } = this.state;
+  render () {
+    console.log('this.props', this.props);
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
+        {this.props.jsx}
         <div className="left-block">
           <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={{ fontSize: 25 }}>{title}</div>
-          <div style={{ color: '#777' }}>Rs {price} </div>
-          <div style={{ color: '#777' }}>Qty: {qty} </div>
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
           <div className="cart-item-actions">
             {/* Buttons */}
             <img
